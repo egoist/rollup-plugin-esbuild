576845a52063de6af34686eb138b17a6b4f5182f
Jay <aelbore.sg@gmail.com>
2020-05-11 11:23:40 +0800

    chore: fixed example/rollup.config.js, add build.ts script (#2)

--- example/rollup.config.js
+++ example/rollup.config.js
@@ -1,11 +1,11 @@
-const esbuild = require('../src/index')
+const esbuild = require('../dist/index')
 
 const isDev = process.env.NODE_ENV === 'development'
 
 export default {
   input: 'example/index.js',
   output: {
-    file: 'dist/index.js',
+    file: 'example/dist/index.js',
     format: 'cjs',
   },
   plugins: [
---
+++ scripts/build.ts
@@ -0,0 +1,34 @@
+import { builtinModules } from 'module'
+import { rollup } from 'rollup'
+
+import dts from 'rollup-plugin-dts'
+import esbuild from '../src/index'
+
+const pkg: { [k: string]: any } = require('../package.json')
+const deps = Object.keys(pkg.dependencies)
+
+;(async function () {
+  const external = [...deps, ...builtinModules]
+
+  async function build() {
+    const bundle = await rollup({
+      input: './src/index.ts',
+      external,
+      plugins: [esbuild()],
+    })
+
+    await bundle.write({ file: './dist/index.js', format: 'cjs' })
+  }
+
+  async function createDtsFile() {
+    const bundle = await rollup({
+      input: './src/index.ts',
+      external,
+      plugins: [dts()],
+    })
+
+    await bundle.write({ file: './dist/index.d.ts' })
+  }
+
+  await Promise.all([build(), createDtsFile()])
+})()
